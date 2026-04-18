e1a34b9127f0ad0fbfc3f7a1174d71956a4e01d2
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2026-04-17 23:14:39 -0400

    Time: 0 ms (100%), Space: 53.6 MB (100%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.js
@@ -0,0 +1,9 @@
+// Added using AI
+var mirrorDistance = function(n) {
+    const revnum = (a) => {
+        if (Math.floor(a / 10) === 0) return a ;
+        const rev = String(a).split('').reverse().join('') ;
+        return parseInt(rev, 10) ;
+    };
+    return Math.abs(n - revnum(n)) ;
+};
