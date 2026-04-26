4f58920ef3673cff311d8e4b17b949d6c6d7dc68
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2026-04-26 15:38:24 -0400

    Time: 1422 ms (7.69%), Space: 105.2 MB (23.08%) - LeetHub

---
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.js
@@ -0,0 +1,54 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestBalanced = function(s) {
+    const n = s.length;
+    let a = 0, b = 0, c = 0;
+    let ans = 0;
+
+    let run = 0, prev = '';
+    for (let i = 0; i < n; ++i) {
+        if (i === 0 || s[i] !== prev) run = 1;
+        else run++;
+        prev = s[i];
+        ans = Math.max(ans, run);
+    }
+
+    const map3 = new Map();
+    const map_ab_c = new Map();
+    const map_ac_b = new Map();
+    const map_bc_a = new Map();
+
+    function key(x,y){ return x + '#' + y; }
+
+    map3.set(key(0,0), 0);
+    map_ab_c.set(key(0,0), 0);
+    map_ac_b.set(key(0,0), 0);
+    map_bc_a.set(key(0,0), 0);
+
+    for (let p = 1; p <= n; ++p) {
+        const ch = s[p-1];
+        if (ch === 'a') a++;
+        else if (ch === 'b') b++;
+        else c++;
+
+        const k3 = key(b - a, c - a);
+        if (map3.has(k3)) ans = Math.max(ans, p - map3.get(k3));
+        else map3.set(k3, p);
+
+        const kabc = key(b - a, c);
+        if (map_ab_c.has(kabc)) ans = Math.max(ans, p - map_ab_c.get(kabc));
+        else map_ab_c.set(kabc, p);
+
+        const kacb = key(c - a, b);
+        if (map_ac_b.has(kacb)) ans = Math.max(ans, p - map_ac_b.get(kacb));
+        else map_ac_b.set(kacb, p);
+
+        const kbc = key(c - b, a);
+        if (map_bc_a.has(kbc)) ans = Math.max(ans, p - map_bc_a.get(kbc));
+        else map_bc_a.set(kbc, p);
+    }
+
+    return ans;
+};
