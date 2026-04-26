fa6b98dcc422486366d739245621438aefc552cc
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2026-04-26 15:37:41 -0400

    Time: 58 ms (10.74%), Space: 55.7 MB (11.36%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,16 @@
+const reverseBits = n => {
+    const rev = (v, len) => {
+        if (len === 1)
+            return v & 1;
+
+        const half = len >> 1;
+        const mask = (1 << half) - 1;
+
+        const lo = v & mask;
+        const hi = v >>> half;
+
+        return (rev(lo, half) << half) | rev(hi, half);
+    };
+
+    return rev(n, 32) >>> 0;
+};
