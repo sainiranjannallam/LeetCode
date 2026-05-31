d75ef53d9729dc0ec628110383cee1a7fa2100a8
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2026-05-31 16:27:56 -0400

    Time: 1 ms (78.24%), Space: 57.5 MB (21.67%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.js
@@ -0,0 +1,16 @@
+var minElement = function(nums) {
+    let minVal = Infinity;
+
+    for (let num of nums) {
+        let currentSum = 0;
+
+        while (num > 0) {
+            currentSum += num % 10;
+            num = Math.floor(num / 10);
+        }
+
+        minVal = Math.min(minVal, currentSum);
+    }
+
+    return minVal;
+};
